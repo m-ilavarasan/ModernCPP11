4d1a2b10cdbc9a029538855248983768f25944a4
m-ilavarasan <m.ilavarasan@gmail.com>
2019-07-25 19:28:50 +0530

    default, delete keywords added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 /*C++ 11 Concepts are discussed here*/
 
+
+
 #include <iostream>
 #include<string>
 
@@ -8,33 +10,64 @@ using namespace std;
 class Functor{
 
 private:
-    string welcomeTxt;
+
+    string  welcomeTxt;
+
  public:
-
-    Functor(string str):welcomeTxt(str)
+    Functor()  = default; // default constructor
+    
+    Functor(const Functor& fun)// Copy constructor
     {
-
+        this->welcomeTxt = fun.welcomeTxt;
     }
-    void operator() (string name)
+    
+    Functor( Functor&& func) = default;
+    
+    Functor operator=(const Functor &fun) = delete; //assignment constructor
+
+    Functor(string str):welcomeTxt(str){}
+
+    void operator() (string name) // Function call overload
     {
         cout<< welcomeTxt+name<<endl;
     }
-
 };
 
+
+
 void construct(string msg, Functor fntr)
+
 {
+
     fntr(msg);
+
 }
+
 int main()
+
 {
+
     cout << "Hello World!" << endl;
+
+    /* Functor Examples*/
+    /*                  */
+    /*******************/
     Functor fntor("hi");
+
     fntor("Ilavarasan");
 
     Functor fntor1("Hello");
-    fntor1(" Ilavarasan");
 
+    fntor1(" Ilavarasan");
+    
     construct("ilavarasan", Functor(" Wow"));
+    /* *****************/
+    
+    Functor fntor2;
+   // fntor2 = fntor1; //will give compile error since assignment constructor declared as delete
+    Functor funtor3(fntor2);
+
+
     return 0;
+
 }
